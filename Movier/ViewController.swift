fc9371d3085415bd56736949a9635bce309fa95b
Loris Dematini <loris.dematini@yahoo.fr>
2022-09-30 12:08:15 +0200

    Vendredi 30/09 12:07

--- Movier/ViewController.swift
+++ Movier/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleMovier: UILabel!
+    
+    @IBOutlet weak var boutonList: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        boutonList.setImage( UIImage.init(named: "image-cover"), for: .normal)
     }
-
-
 }
-
---
+++ Movier/controller/MovierViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovierViewController.swift
+//  Movier
+//
+//  Created by Loris Dematini on 30/09/2022.
+//
+import Foundation
+import UIKit
+
+class MovierViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
